337fed52b27a5d12947f115f214daf79ae0ef46d
Yurii Karabas <1998uriyyo@gmail.com>
2021-01-16 15:33:30 +0200

    🎉 Add ability to get feed caption (#84)

--- instapi/models/media.py
+++ instapi/models/media.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import cast
 
+from ..cache import cached
 from ..client import client
 from ..types import StrDict
 from .base import Entity
@@ -8,6 +9,7 @@ from .base import Entity
 
 @dataclass(frozen=True)
 class Media(Entity):
+    @cached
     def _media_info(self) -> StrDict:
         items, *_ = client.media_info(self.pk)["items"]
         return cast(StrDict, items)
